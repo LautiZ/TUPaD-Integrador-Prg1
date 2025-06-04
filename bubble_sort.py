23b3c54735ecdc18787e624748e004fade0769e1
ignacio salazar <ignaciosalazar986@gmail.com>
2025-06-04 18:17:35 -0300

    Se explica el paso a paso de manera clara de la funcion bubble_sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,21 +1,10 @@
 def bubble_sort(arr):
-  
-    # Outer loop to iterate through the list n times
+    # Se recorre la lista de atras hacia delante
     for n in range(len(arr) - 1, 0, -1):
-        
-        # Initialize swapped to track if any swaps occur
-        swapped = False  
-
-        # Inner loop to compare adjacent elements
-        for i in range(n):
-            if arr[i] > arr[i + 1]:
-              
-                # Swap elements if they are in the wrong order
-                arr[i], arr[i + 1] = arr[i + 1], arr[i]
-                
-                # Mark that a swap has occurred
-                swapped = True
-        
-        # If no swaps occurred, the list is already sorted
-        if not swapped:
-            break
+        swapped = False  #Se mantiene en false en cada nueva iteracion para verificar si se han efectuado cambios
+        for i in range(n): # Se recorre hasta el indice "n" haciendo un bucle cada vez mas pequeño
+            if arr[i] > arr[i + 1]: #Se compara que elemento es mayor
+                arr[i], arr[i + 1] = arr[i + 1], arr[i] # De cumplirse la condicion se realiza un intercambio de valores
+                swapped = True # Se confirma que hubo cambios
+        if not swapped: # Cuando la variable swapped deje de cambiar su estado significa que el ordenamiento ha finalizado
+            break # Se termina el ciclo
