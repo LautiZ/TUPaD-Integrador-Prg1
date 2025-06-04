d7b813d0d8629b16d40ca91c851a32b0fb03f504
ignacio salazar <ignaciosalazar986@gmail.com>
2025-06-04 17:46:20 -0300

    se obtiene fn bubble_sort de https://www.geeksforgeeks.org/python-program-for-bubble-sort/

--- ordenamiento.py
+++ ordenamiento.py
@@ -1,14 +1,29 @@
 my_list = [5, 4, 23, 2, 1, 45, 212, 32, 54]  # Lista para realizar primera prueba
 
 
-def bubble_sort(arr): #Funcion eficiente para ordenamientos pequeños y semi ordenados
-    n = len(arr)
-    for i in range(n):
-        for j in range(0, n - i - 1):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+def bubble_sort(arr):
+  
+    # Outer loop to iterate through the list n times
+    for n in range(len(arr) - 1, 0, -1):
+        
+        # Initialize swapped to track if any swaps occur
+        swapped = False  
 
+        # Inner loop to compare adjacent elements
+        for i in range(n):
+            if arr[i] > arr[i + 1]:
+              
+                # Swap elements if they are in the wrong order
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                
+                # Mark that a swap has occurred
+                swapped = True
+        
+        # If no swaps occurred, the list is already sorted
+        if not swapped:
+            break
 
+        
 def quicksort(arr): # Funcion eficiente para ordenamientos mayores
     if len(arr) <= 1:
         return arr
