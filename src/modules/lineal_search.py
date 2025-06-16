404cbb9ef13d63730ef44236e64249ef95343565
ignacio salazar <ignaciosalazar986@gmail.com>
2025-06-16 19:02:43 -0300

    comentarios

--- src/modules/lineal_search.py
+++ src/modules/lineal_search.py
@@ -1,5 +1,7 @@
-def busqueda_lineal(lista, objetivo):
-    for i in range(len(lista)):
-        if lista[i] == objetivo:
-            return i
-    return -1
+def busqueda_lineal(lista, objetivo): 
+    # Recorre todos los elementos de la lista uno por uno
+    for i in range(len(lista)): 
+        # Compara el elemento actual con el objetivo
+        if lista[i] == objetivo:  # Si encuentra el objetivo, devuelve su índice y finaliza la búsqueda
+            return i 
+    return -1 #Esta linea se ejecuta solo si no se ha localizado el objetivo dentro de la lista
